b9d55b4b74dff4319a79e81285a916cc385cc104
Himanshu Goyal <himanshu.goyal@appdirect.com>
2021-06-11 10:25:09 +0530

    Created Entity related to catalog

---
+++ src/main/java/com/omnirio/catalogapplication/model/CategoryAttributes.java
@@ -0,0 +1,44 @@
+package com.omnirio.catalogapplication.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@Data
+@Builder
+@DynamicUpdate
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "category_attributes")
+public class CategoryAttributes extends Auditable {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category;
+
+	@Column(name = "uuid", nullable = false, unique = true)
+	private String uuid;
+
+	@Column(name = "attribute_name", nullable = false)
+	private String attributeName;
+
+	@Column(name = "attribute_value", nullable = false)
+	private String attributeValue;
+}
---
+++ src/main/java/com/omnirio/catalogapplication/model/ProductAttributes.java
@@ -0,0 +1,44 @@
+package com.omnirio.catalogapplication.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@Data
+@Builder
+@DynamicUpdate
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "product_attributes")
+public class ProductAttributes extends Auditable {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@Column(name = "uuid", nullable = false, unique = true)
+	private String uuid;
+
+	@Column(name = "attribute_name", nullable = false)
+	private String attributeName;
+
+	@Column(name = "attribute_value", nullable = false)
+	private String attributeValue;
+}
